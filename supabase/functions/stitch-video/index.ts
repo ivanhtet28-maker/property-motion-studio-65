ca339b041162d5c29190a99df205056aeca8fc43
Mr-S-26 <michaelryansia26@gmail.com>
2026-02-10 09:58:49 +0800

    Remove position: absolute from agent card HTML to match working property details structure

--- supabase/functions/stitch-video/index.ts
+++ supabase/functions/stitch-video/index.ts
@@ -302,9 +302,6 @@
                     type: "html",
                     html: `
                       <div style="
-                        position: absolute;
-                        top: 0;
-                        left: 0;
                         width: 100%;
                         height: 100%;
                         display: flex;
